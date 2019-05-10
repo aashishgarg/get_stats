cb33dec2f4cb0b197ea1aefc15ee74dcf69ff86f
Ashish Garg <ashish.kumar2@rsystems.com>
2019-05-10 12:19:58 +0530

    attr_accessors corrected

--- base.rb
+++ base.rb
@@ -1,6 +1,4 @@
 module Stats
   class Base
-    # --- Attribute Accessors
-    attr_accessor :processes, :result, :repositories, :ports, :pids, :release, :command, :sanitizer
   end
 end
--- instance.rb
+++ instance.rb
@@ -6,7 +6,7 @@ require_relative './server/linux/sanitizer'
 module Stats
   class Instance < Base
     # --- Attribute Accessors
-    attr_accessor :server, :result
+    attr_accessor :processes, :server, :result, :repositories, :ports, :pids, :release, :command, :sanitizer
 
     def initialize(type)
       @command = get_command_obj
@@ -14,12 +14,13 @@ module Stats
       @sanitizer = get_sanitizer_obj
       @release = command.release
       @result = { server_release: sanitizer.perform(release), processes: [] }
-      @server = get_instance
-      @server.build_result
+      @server = get_instance(type)
+      @server.build_result if server
     end
 
-    def get_instance
-      Server::Linux::Instance.new(processes, sanitizer, command, result)
+    def get_instance(type)
+      instance_type = type == 'Linux' ? Server::Linux::Instance : nil
+      instance_type.nil? ? nil : instance_type.new(processes, sanitizer, command, result)
     end
 
     def get_command_obj
