fcfd11d4499f4fa5f714c860c3f9d1dc3f7d8855
Ashish Garg <ashish.kumar2@rsystems.com>
2019-05-10 16:25:22 +0530

    extra commands added

--- server/linux/command.rb
+++ server/linux/command.rb
@@ -8,8 +8,17 @@ module Stats
           `lsb_release -ds ` + `/bin/uname -r`.chomp
         end
 
+        def app_servers_pids
+          `lsof -wni | grep ruby | grep IPv4 | awk '{print $2,$9}'`
+        end
+
+        def redis_server_pids
+          `sudo -A netstat -nlp | grep redis | grep 'tcp' | awk '{print $7,$4 }'`
+        end
+
+        # Tested for [Passenger, puma, thin, webrick]
         def active_processes
-          # `export SUDO_ASKPASS=#{ask_pass_path};sudo -A netstat -nlp | grep #{app_server} | grep 'tcp' | awk '{print $4, $7 }'`
+          # `export SUDO_ASKPASS=#{ask_pass_path};sudo -A netstat -nlp | grep #{app_server} | grep 'tcp' | awk '{print $7,$4 }'`
           `lsof -wni | grep ruby | awk '{print $2,$9}'`
         end
 
